c5e89f114eb83d2ab74421e61ea6590a124a3703
kitakitsune <kitakitsune@skiff.com>
2023-12-05 23:38:52 -0500

    handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,37 @@
 'use client';
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+import axios from "axios";
+import { useForm, Controller } from "react-hook-form";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const {register, control, handleSubmit} = useForm<IssueForm>();
+
   return (
-    <div className='max-w-xl space-y-3'>
+    <form 
+    className='max-w-xl space-y-3' 
+    onSubmit={handleSubmit(async (data) => {
+      await axios.post('/api/issues', data);
+      router.push('/issues')
+    })}>
         <TextField.Root>
-          <TextField.Input placeholder='Title' />
+          <TextField.Input placeholder='Title' {...register('title')} />
         </TextField.Root>
-        <TextArea placeholder='Description' />
+        <Controller 
+          name='description'
+          control={control}
+          render={({ field }) => <SimpleMDE placeholder='Description' {...field}/>}
+        />
         <Button>Submit New Issue</Button>
-    </div>
+    </form>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,16 @@ import '@radix-ui/themes/styles.css';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Theme, ThemePanel } from '@radix-ui/themes'
-import './globals.css'
-import NavBar from './NavBar'
+import '@radix-ui/themes/styles.css';
+import './theme-config.css';
+import './globals.css';
+import NavBar from './NavBar';
 
-const inter = Inter({ subsets: ['latin'] })
+
+const inter = Inter({ 
+  subsets: ['latin'], 
+  variable: '--font-inter'
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,7 +25,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.variable}>
         <Theme accentColor="mint" grayColor="olive">
           <NavBar />
           <main className='p-5'>
